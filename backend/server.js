d1bb0aff0e6e333da1648391e79f4f4841131584
Syed Muhammad Mehdi <syedmuhammadmehdi05@gmail.com>
2026-06-23 21:48:16 +0500

    Add request logger and ensure health endpoint

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,11 @@ const orderRoutes = require("./routes/orderRoutes");
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+    console.log(`📡 ${req.method} ${req.url}`);
+    next();
+});
+
 // ===== STATIC FILES =====
 app.use(express.static(path.join(__dirname, '../frontend')));
 
